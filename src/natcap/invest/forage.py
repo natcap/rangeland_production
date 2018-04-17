f79444e83808eec6a36405d36f34c502bef622fa
Ginger <ginger.kowal@gmail.com>
2018-04-17 10:55:48 -0400

    check for min and max temperature, pft and animal traits corresponding to spatial inputs Branch: feature/FORAGE-MODEL-2-sample-inputs-testing

--- src/natcap/invest/forage.py
+++ src/natcap/invest/forage.py
@@ -6,7 +6,12 @@ https://docs.google.com/document/d/10oJo43buEdJkFTZ0wYaW00EagSzs1oM7g_lBUc8URMI/
 import os
 import logging
 
+from osgeo import ogr
+from osgeo import gdal
+import re
+
 import pygeoprocessing
+from natcap.invest import utils
 
 LOGGER = logging.getLogger('natcap.invest.forage')
 
@@ -32,6 +37,7 @@ def execute(args):
             clipping the computational area of the input datasets to be the
             area intersected by this polygon.
         args['bulk_density_path'] (string): path to bulk density raster.
+        args['ph_path'] (string): path to soil pH raster.
         args['clay_proportion_path'] (string): path to raster representing
             per-pixel proportion of soil component that is clay
         args['silt_proportion_path'] (string): path to raster representing
@@ -47,17 +53,29 @@ def execute(args):
             expect to find files named
                  "./precip_dir/chirps-v2.0.2016.05.tif" to
                  "./precip_dir/chirps-v2.0.2018.09.tif"
-        args['monthly_temperature_path_pattern'] (string): path to monthly
+        args['monthly_min_temperature_path_pattern'] (string): path to monthly
             temperature data pattern where <month> can be replaced with the
             number 1..12 when the simultation needs a monthly temperature
             input
-        args['veg_spatial_composition_path'] (string): path to vegetation
-            spatial composition raster. AT THE MOMENT I DON'T KNOW WHAT THE
-            EXPECTED VALUES/UNITS OF THIS RASTER ARE.
-        args['animal_inputs_path'] (string): path to animal vector inputs.
-            Should have the following fields:
-                'density': WHAT ARE THE UNITS
-                ANY MORE FIELDS?
+        args['monthly_max_temperature_path_pattern'] (string): path to monthly
+            temperature data pattern where <month> can be replaced with the
+            number 1..12 when the simultation needs a monthly temperature
+            input
+        args['veg_trait_path'] (string): path to csv file giving vegetation
+			traits for each plant functional type available for grazing. This
+			file must be indexed by a unique integer field "PFT" that
+            corresponds to veg spatial composition rasters.            
+		args['veg_spatial_composition_path_pattern'] (string): path to
+			vegetation rasters, one per plant functional type available for
+			grazing, where <PFT> can be replaced with an integer that is
+			indexed in the veg trait csv.
+        args['animal_trait_path'] string: path to csv file giving animal traits
+            for each animal type - number - duration combination, indexed to
+            the animal management layer by the unique integer field
+            "animal_id".
+        args['animal_mgmt_layer_path'] (string): path to animal vector inputs
+            giving the location of grazing animals. Must be indexed by the
+            field "animal_id" that corresponds to the animal trait csv.
 
     Returns:
         None.
@@ -95,29 +113,82 @@ def execute(args):
             "\n\t".join(missing_precip_path_list))
 
     # this list will be used to record any expected files that are not found
-    missing_temperature_path_list = []
+    missing_min_temperature_path_list = []
     for month_i in temperature_month_set:
-        temp_path = args['monthly_temperature_path_pattern'].replace(
+        temp_path = args['monthly_min_temperature_path_pattern'].replace(
             '<month>', '%.2d' % month_i)
-        base_align_raster_path_id_map['temp_%d' % month_i] = temp_path
+        base_align_raster_path_id_map['min_temp_%d' % month_i] = temp_path
         if not os.path.exists(temp_path):
-            missing_temperature_path_list.append(temp_path)
-
-    if missing_temperature_path_list:
+            missing_min_temperature_path_list.append(temp_path)
+    if missing_min_temperature_path_list:
         raise ValueError(
             "Couldn't find the following temperature raster paths given the " +
-            "pattern: %s\n\t" % args['monthly_temperature_path_pattern'] +
-            "\n\t".join(missing_temperature_path_list))
+            "pattern: %s\n\t" % args['monthly_min_temperature_path_pattern'] +
+            "\n\t".join(missing_min_temperature_path_list))
+    
+    missing_max_temperature_path_list = []
+    for month_i in temperature_month_set:
+        temp_path = args['monthly_max_temperature_path_pattern'].replace(
+            '<month>', '%.2d' % month_i)
+        base_align_raster_path_id_map['max_temp_%d' % month_i] = temp_path
+        if not os.path.exists(temp_path):
+            missing_max_temperature_path_list.append(temp_path)
+    if missing_max_temperature_path_list:
+        raise ValueError(
+            "Couldn't find the following temperature raster paths given the " +
+            "pattern: %s\n\t" % args['monthly_max_temperature_path_pattern'] +
+            "\n\t".join(missing_max_temperature_path_list))
 
     # lookup to provide path to soil percent given soil type
     for soil_type in SOIL_TYPE_LIST:
         base_align_raster_path_id_map[soil_type] = (
-            args['%s_percent_path' % soil_type])
+            args['%s_proportion_path' % soil_type])
         if not os.path.exists(base_align_raster_path_id_map[soil_type]):
             raise ValueError(
                 "Couldn't find %s for %s" % (
                     base_align_raster_path_id_map[soil_type], soil_type))
+    base_align_raster_path_id_map['bulk_d'] = args['bulk_density_path']
+    base_align_raster_path_id_map['ph'] = args['ph_path']
 
+    # make sure veg traits exist for each pft raster
+    pft_dir = os.path.dirname(args['veg_spatial_composition_path_pattern'])
+    pft_basename = os.path.basename(
+        args['veg_spatial_composition_path_pattern'])
+    files = [f for f in os.listdir(pft_dir) if os.path.isfile(
+             os.path.join(pft_dir, f)) and f.endswith(pft_basename[-4:])]
+    pft_list = [int(re.search(pft_basename.replace('<PFT>', '(.+?)'),
+                    f).group(1)) for f in files]
+    for pft_i in pft_list:
+        pft_path = args['veg_spatial_composition_path_pattern'].replace(
+            '<PFT>', '%d' % pft_i)
+        base_align_raster_path_id_map['pft_%d' % pft_i] = pft_path
+    veg_trait_table = utils.build_lookup_from_csv(args['veg_trait_path'],
+                                                  'PFT')
+    missing_pft_trait_list = set(pft_list).difference(
+        set(veg_trait_table.keys()))
+    if missing_pft_trait_list:
+        raise ValueError(
+            "Couldn't find trait values for the following plant functional " +
+            "types: %s\n\t" + ", ".join(missing_pft_trait_list))
+            
+    # make sure animal traits exist for each feature in animal management
+    # layer
+    anim_id_list = []
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    datasource = driver.Open(args['animal_mgmt_layer_path'], 0)
+    layer = datasource.GetLayer()
+    for feature in layer:
+        anim_id_list.append(feature.GetField('animal_id'))
+    
+    anim_trait_table = utils.build_lookup_from_csv(args['animal_trait_path'],
+                                                   'animal_id')
+    missing_animal_trait_list = set(anim_id_list).difference(
+        set(anim_trait_table.keys()))
+    if missing_animal_trait_list:
+        raise ValueError(
+            "Couldn't find trait values for the following animal " +
+            "ids: %s\n\t" + ", ".join(missing_animal_trait_list))
+            
     # find the smallest target_pixel_size
     target_pixel_size = min(*[
         pygeoprocessing.get_raster_info(path)['pixel_size']
