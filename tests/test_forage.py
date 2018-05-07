d641e9777315fd38dd5ecd06702959b93335ea08
Ginger <ginger.kowal@gmail.com>
2018-05-07 18:04:52 -0400

    changes to sample inputs generated to match runs with old forage model Branch: feature/FORAGE-MODEL-3-initialization

--- tests/test_forage.py
+++ tests/test_forage.py
@@ -8,8 +8,7 @@ import sys
 
 
 SAMPLE_DATA = "C:/Users/ginge/Documents/NatCap/sample_inputs"
-# REGRESSION_DATA = ????  TODO collect results to use as regression data
-# waiting for that until we know what Century variables should be fixed
+REGRESSION_DATA = "C:/Users/ginge/Documents/NatCap/regression_test_data"
 
 class foragetests(unittest.TestCase):
     """Regression tests for InVEST forage model."""
@@ -27,11 +26,12 @@ class foragetests(unittest.TestCase):
         """Generate a base sample args dict for forage model."""
         args = {
             'workspace_dir': workspace_dir,
+            'results_suffix': "",
             'starting_month': 1,
             'starting_year': 2016,
             'n_months': 22,
             'aoi_path': os.path.join(
-                SAMPLE_DATA, 'soums_monitoring_area.shp'),
+                SAMPLE_DATA, 'Manlai_soum.shp'),
             'bulk_density_path': os.path.join(SAMPLE_DATA,
                 'soil', 'bulkd.tif'),
             'ph_path': os.path.join(SAMPLE_DATA,
@@ -43,11 +43,16 @@ class foragetests(unittest.TestCase):
             'sand_proportion_path': os.path.join(SAMPLE_DATA,
                 'soil', 'sand.tif'),
             'monthly_precip_path_pattern': os.path.join(
-                SAMPLE_DATA, 'precip', 'chirps-v2.0.<year>.<month>.tif'),
+                SAMPLE_DATA, 'CHIRPS_div_by_10',
+                'chirps-v2.0.<year>.<month>.tif'),
             'min_temp_path_pattern': os.path.join(
                 SAMPLE_DATA, 'temp', 'wc2.0_30s_tmin_<month>.tif'),
             'max_temp_path_pattern': os.path.join(
                 SAMPLE_DATA, 'temp', 'wc2.0_30s_tmax_<month>.tif'),
+            'site_param_table': os.path.join(SAMPLE_DATA,
+                'site_parameters.csv'),
+            'site_param_spatial_index_path': os.path.join(
+                SAMPLE_DATA, 'site_index.tif'),
             'veg_trait_path': os.path.join(SAMPLE_DATA, 'pft_trait.csv'),
             'veg_spatial_composition_path_pattern': os.path.join(
                 SAMPLE_DATA, 'pft<PFT>.tif'),
@@ -55,6 +60,8 @@ class foragetests(unittest.TestCase):
                 SAMPLE_DATA, 'animal_trait_table.csv'),
             'animal_mgmt_layer_path': os.path.join(
                 SAMPLE_DATA, 'sheep_units_density_2016_monitoring_area.shp'),
+            'initial_conditions_dir': os.path.join(
+                SAMPLE_DATA, 'initialization_data'),
         }
         return args
         
