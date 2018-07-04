ffd3c79eaaed95c153e8e9c5aaa626779ccdd9a8
Ginger <ginger.kowal@gmail.com>
2018-07-04 09:25:48 -0400

    added test for calculation of shortwave radiation Branch: feature/FORAGE-MODEL-4-one-step

--- tests/test_forage.py
+++ tests/test_forage.py
@@ -6,21 +6,28 @@ import shutil
 import os
 import sys
 
+import numpy
+from osgeo import osr
+from osgeo import gdal
+
+import pygeoprocessing
 
 SAMPLE_DATA = "C:/Users/ginge/Documents/NatCap/sample_inputs"
 REGRESSION_DATA = "C:/Users/ginge/Documents/NatCap/regression_test_data"
+_TARGET_NODATA = -1.0
+
 
 class foragetests(unittest.TestCase):
     """Regression tests for InVEST forage model."""
-    
+
     def setUp(self):
         """Create temporary workspace directory."""
         self.workspace_dir = tempfile.mkdtemp()
-        
+
     def tearDown(self):
         """Clean up remaining files."""
         shutil.rmtree(self.workspace_dir)
-    
+
     @staticmethod
     def generate_base_args(workspace_dir):
         """Generate a base sample args dict for forage model."""
@@ -32,16 +39,16 @@ class foragetests(unittest.TestCase):
             'n_months': 22,
             'aoi_path': os.path.join(
                 SAMPLE_DATA, 'Manlai_soum_WGS84.shp'),
-            'bulk_density_path': os.path.join(SAMPLE_DATA,
-                'soil', 'bulkd.tif'),
-            'ph_path': os.path.join(SAMPLE_DATA,
-                'soil', 'phihox_sl3.tif'),
-            'clay_proportion_path': os.path.join(SAMPLE_DATA,
-                'soil', 'clay.tif'),
-            'silt_proportion_path': os.path.join(SAMPLE_DATA,
-                'soil', 'silt.tif'),
-            'sand_proportion_path': os.path.join(SAMPLE_DATA,
-                'soil', 'sand.tif'),
+            'bulk_density_path': os.path.join(
+                SAMPLE_DATA, 'soil', 'bulkd.tif'),
+            'ph_path': os.path.join(
+                SAMPLE_DATA, 'soil', 'phihox_sl3.tif'),
+            'clay_proportion_path': os.path.join(
+                SAMPLE_DATA, 'soil', 'clay.tif'),
+            'silt_proportion_path': os.path.join(
+                SAMPLE_DATA, 'soil', 'silt.tif'),
+            'sand_proportion_path': os.path.join(
+                SAMPLE_DATA, 'soil', 'sand.tif'),
             'monthly_precip_path_pattern': os.path.join(
                 SAMPLE_DATA, 'CHIRPS_div_by_10',
                 'chirps-v2.0.<year>.<month>.tif'),
@@ -49,8 +56,8 @@ class foragetests(unittest.TestCase):
                 SAMPLE_DATA, 'temp', 'wc2.0_30s_tmin_<month>.tif'),
             'max_temp_path_pattern': os.path.join(
                 SAMPLE_DATA, 'temp', 'wc2.0_30s_tmax_<month>.tif'),
-            'site_param_table': os.path.join(SAMPLE_DATA,
-                'site_parameters.csv'),
+            'site_param_table': os.path.join(
+                SAMPLE_DATA, 'site_parameters.csv'),
             'site_param_spatial_index_path': os.path.join(
                 SAMPLE_DATA, 'site_index.tif'),
             'veg_trait_path': os.path.join(SAMPLE_DATA, 'pft_trait.csv'),
@@ -65,14 +72,69 @@ class foragetests(unittest.TestCase):
                 SAMPLE_DATA, 'initialization_data'),
         }
         return args
-        
+
     def test_model_runs(self):
-        """Launch forage model, ensure it runs!"""
+        """Launch forage model, ensure it runs!."""
         from natcap.invest import forage
-        
+
         if not os.path.exists(SAMPLE_DATA):
             self.fail(
                 "Sample input directory not found at %s" % SAMPLE_DATA)
-                
+
         args = foragetests.generate_base_args(self.workspace_dir)
         forage.execute(args)
+
+    def test_shortwave_radiation(self):
+        """Test calculation of shortwave radiation outside the atmosphere.
+
+        Use one set of inputs, including a raster with known latitude, to test
+        the function `_shortwave radiation` against a result calculated by
+        hand.
+
+        Raises:
+            AssertionError if the raster created by `_shortwave_radiation`
+                contains more than one unique value
+            AssertionError if the value returned by `_shortwave_radiation' is
+                not within 0.01 of the value calculated by hand
+
+        Returns:
+            None
+        """
+        from natcap.invest import forage
+        fill_value = 0
+        template_raster = os.path.join(
+            self.workspace_dir, 'template_raster.tif')
+
+        # create_test_raster of 1 pixel
+        geotransform = [0, 1, 0, 44.5, 0, 1]
+        n_cols = 1
+        n_rows = 1
+        n_bands = 1
+        datatype = gdal.GDT_Float32
+        projection = osr.SpatialReference()
+        projection.SetWellKnownGeogCS('WGS84')
+        driver = gdal.GetDriverByName('GTiff')
+        target_raster = driver.Create(
+            template_raster.encode('utf-8'), n_cols, n_rows, n_bands, datatype)
+        target_raster.SetProjection(projection.ExportToWkt())
+        target_raster.SetGeoTransform(geotransform)
+        target_band = target_raster.GetRasterBand(1)
+        target_band.SetNoDataValue(_TARGET_NODATA)
+        target_band.Fill(fill_value)
+        target_raster = None
+
+        month = 5
+        shwave_path = os.path.join(self.workspace_dir, 'shwave.tif')
+        forage._shortwave_radiation(template_raster, month, shwave_path)
+
+        # assert the value in the raster `shwave_path` is equal to value
+        # calculated by hand
+        result_set = set()
+        for offset_map, raster_block in pygeoprocessing.iterblocks(
+                shwave_path):
+            result_set.update(numpy.unique(raster_block))
+        assert len(result_set) == 1, """One unique value expected in shortwave
+            radiation raster"""
+        test_result = list(result_set)[0]
+        assert abs(test_result - 990.7401) < 0.01, """Test result does not
+            match expected value"""
