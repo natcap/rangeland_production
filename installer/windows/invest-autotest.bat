368df69eb34e6d7b57e77ba296fa75a05ffb115e
Ginger <ginger.kowal@gmail.com>
2019-09-09 16:21:46 -0400

    remove InVEST installer items Branch: feature/UI-1-new-gui-windows-installer

--- installer/windows/invest-autotest.bat
+++
@@ -1,6 +0,0 @@
-@REM Run this script from within the invest-3-x86 directory to run the
-@REM invest-autotest program on windows.
-@REM
-@REM Assumes that python is on the PATH.
-
-python "%CD%\invest-autotest.py" --cwd=".." --binary="%CD%\invest.exe
