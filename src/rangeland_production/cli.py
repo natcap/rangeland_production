0eb048681a6c36459684ef8eabfcbef4f43f8b14
vakowal <vakowal@ncsu.edu>
2020-01-06 13:41:08 -0500

    remove import of sip and cast string to boolean to resolve typeerror on application exit

--- src/rangeland_production/cli.py
+++ src/rangeland_production/cli.py
@@ -114,22 +114,6 @@ def main(user_args=None):
     # Now that we've set up logging based on args, we can start logging.
     LOGGER.debug(args)
 
-    try:
-        # Importing model UI files here will usually import qtpy before we can
-        # set the sip API in natcap.invest.ui.inputs.
-        # Set it here, before we can do the actual importing.
-        import sip
-        # 2 indicates SIP/Qt API version 2
-        sip.setapi('QString', 2)
-
-        from rangeland_production.ui import inputs
-    except ImportError as error:
-        # Can't import UI, exit with nonzero exit code
-        LOGGER.exception('Unable to import the UI')
-        parser.error(('Unable to import the UI (failed with "%s")\n'
-                      'Is the UI installed?\n'
-                      '    pip install rangeland_production[ui]') % error)
-
     if args.headless:
         from rangeland_production import datastack
         target_mod = _PYNAME
@@ -212,6 +196,7 @@ def main(user_args=None):
             # execute the model's execute function with the loaded args
             getattr(model_module, 'execute')(paramset.args)
     else:
+        from rangeland_production.ui import inputs
         # import the GUI from the known class
         gui_class = _GUI_CLASS
         module_name, classname = gui_class.split('.')
